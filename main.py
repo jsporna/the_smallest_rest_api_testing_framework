11d7990c77c4a030933749e49e325db7ddad6c0c
Jakub Spórna <jakub.sporna@gmail.com>
2020-12-18 22:39:16 +0100

    updated main function with 10 lines of code

--- main.py
+++ main.py
@@ -2,14 +2,9 @@ import unittest, xmlrunner, json, requests
 
 class Tests(unittest.TestCase): pass
 
-def add_test(cls, name, data):
-    def abstract_test(self):
-        self.assertEqual(requests.request(**data['request']).status_code, data['assert']['statusCode'])
-    setattr(cls, name, abstract_test)
-
 with open('tests.json', 'r') as jfile:
     for test_name, test_data in json.load(jfile).items():
-        add_test(Tests, test_name, test_data)
+        setattr(Tests, test_name, lambda self: self.assertEqual(requests.request(**test_data['request']).status_code, test_data['assert']['statusCode']))
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner())
