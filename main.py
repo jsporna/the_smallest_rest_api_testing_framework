3159a4112672db4ff349f457487fcf9818f2b52a
Jakub Spórna <jakub.sporna@gmail.com>
2020-12-21 17:10:41 +0100

    fixed lambda function generating same body for each iteration step

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import unittest, xmlrunner, json, requests
+import unittest, xmlrunner, json, requests, copy
 
 class Tests(unittest.TestCase): pass
 
 with open('tests.json', 'r') as jfile:
     for test_name, test_data in json.load(jfile).items():
-        setattr(Tests, test_name, lambda self: self.assertEqual(requests.request(**test_data['request']).status_code, test_data['assert']['statusCode']))
+        func = lambda data: lambda self: self.assertEqual(requests.request(**data['request']).status_code, data['assert']['statusCode'])
+        setattr(Tests, test_name, func(test_data))
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner())
