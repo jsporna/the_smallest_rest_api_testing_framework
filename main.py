27b6db06bb35e8dca7102b529e7200d6e17ac6dd
Jakub Spórna <jakub.sporna@gmail.com>
2020-12-22 09:36:00 +0100

    moved definition of lambdas into setattr function

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import unittest, xmlrunner, json, requests
 class Tests(unittest.TestCase): pass
 with open('tests.json', 'r') as jfile:
     for test_name, test_data in json.load(jfile).items():
-        func = lambda data: lambda self: self.assertEqual(requests.request(**data['request']).status_code, data['assert']['statusCode'])
-        setattr(Tests, test_name, func(test_data))
+        setattr(Tests, test_name, (lambda data: lambda self: self.assertEqual(requests.request(**data['request']).status_code, data['assert']['statusCode']))(test_data))
 unittest.main(testRunner=xmlrunner.XMLTestRunner())
