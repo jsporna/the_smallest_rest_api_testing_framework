3db678bbb5aaec83b759527824343728a365493e
Jakub Spórna <jakub.sporna@gmail.com>
2020-12-22 09:49:26 +0100

    moved definition class into context body

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import unittest, xmlrunner, json, requests
-class Tests(unittest.TestCase): pass
+
 with open('tests.json', 'r') as jfile:
-    for test_name, test_data in json.load(jfile).items():
-        setattr(Tests, test_name, (lambda data: lambda self: self.assertEqual(requests.request(**data['request']).status_code, data['assert']['statusCode']))(test_data))
+    class Tests(unittest.TestCase): pass
+    for test_name, test_data in json.load(jfile).items(): setattr(Tests, test_name, (lambda data: lambda self: self.assertEqual(requests.request(**data['request']).status_code, data['assert']['statusCode']))(test_data))
+
 unittest.main(testRunner=xmlrunner.XMLTestRunner())
