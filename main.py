d07b35ec9a3d9d04d26c0f13c3aaf82889a1dbcf
Jakub Spórna <jakub.sporna@gmail.com>
2021-01-09 19:07:48 +0100

    replace json.loads with load

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ def abstract_test(self, data, response):
     self.assertLessEqual(response.elapsed.total_seconds(), data['assert']['responseTime'])
 
 test = lambda data: lambda self: abstract_test(self, data, requests.request(**data['request']))
-globals().update({file_name: type(file_name, (unittest.TestCase, ), {name: test(data) for name, data in json.loads(open(file_name, 'r').read()).items() }) for file_name in glob.iglob("test_*.json")})
+globals().update({file_name: type(file_name, (unittest.TestCase, ), {name: test(data) for name, data in json.load(open(file_name, 'r')).items() }) for file_name in glob.iglob("test_*.json")})
 unittest.main(testRunner=xmlrunner.XMLTestRunner())
